36ada8f061fca0924cdffabd6d8956429ac900a8
unknown <developer.m.irfan@gmail.com>
2022-09-14 21:46:54 -0700

    footer added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 
 import { Hero } from '../components/1-Hero/Hero';
+import Footer from '../components/z-Footer/Footer';
 import { Layout } from '../layout/Layout';
 
 const Home = () => {
   return (
     <Layout>
       <Hero/>
+      <Footer />
       </Layout>
+
     
   );
 };

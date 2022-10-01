13f1b78083260e9a752e8e607dfacfef92e1ec8d
l0gicerr0r <alfithomas2002@gmail.com>
2022-10-01 11:29:37 +0530

    intial commit

---
+++ src/components/Content/abcontentStyles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+export const LargeHeading = styled.div``;
+export const HeroContentAction = styled.div`
+height
+width
+
+`;
+export const HeroImage = styled.div`
+position:relative;
+height
+width
+`;
+export const HeroGrid = styled.div``;
+export const HeroParagraph = styled.div``;
+
+
+

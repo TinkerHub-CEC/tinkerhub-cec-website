9a8d6490ab16819ffc13ec4f1b65091e7a3b7f61
Rithin Samuel <rithinsamuel12@gmail.com>
2022-10-01 11:01:15 +0530

    Header component updated

--- src/components/0-Header/Header.js
+++ src/components/0-Header/Header.js
@@ -5,22 +5,19 @@ import { Container,Row,HeaderImage,HeaderContent,L,Headerlogo,LImage,I} from "./
 function Welcome(){
     return(
         <Container>
-            <Row>
-                <HeaderImage src="/images/Logo(Black).png"/>
-
+            <Row>                
+                <I href="#"><HeaderImage src="/images/Logo(Black).png"/></I>
                 
                 <HeaderContent>
                     <Link href="/" passHref><L>Home</L></Link>
                     <Link href="/about" passHref><L>About</L></Link>  
-                    <Link href="/" passHref><L>Event</L></Link>          
+                    <Link href="/events" passHref><L>Event</L></Link>          
                 </HeaderContent>
-
-
 
 
                 <Headerlogo>
                     <I href="https://linktr.ee/Tinkerhub_cec"><LImage src="/images/Linktree.png"/></I>
-                    <I href="#"><LImage src="/images/LinkedIn.png"/></I>
+                    <I href="https://www.linkedin.com/company/tinkerhub-cec/"><LImage src="/images/LinkedIn.png"/></I>
                     <I href="https://www.instagram.com/tinkerhub.cec/"><LImage src="/images/Instagram logo.png"/></I>
                 </Headerlogo>
             </Row>
--- src/components/0-Header/HeaderStyles.js
+++ src/components/0-Header/HeaderStyles.js
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 export const Container = styled.div`
 padding:0px;
 margin:0px;
+max-height:230px;
 `;
 
 export const Row=styled.div`
 display:flex;
 flex-direction:row;
 padding:50px;
-margin-left:20px;
+padding-bottom:0;
+margin-left:70px;
 `;
 
 export const HeaderImage=styled.img`
@@ -20,23 +22,22 @@ height:100px;
 export const HeaderContent=styled.ul`
 margin-left:120px;
 margin-top:30px;
-
 `;
+
 export const L =styled.a`
 text-decoration:none;
 color:black;
-margin-right:100px;
-
+margin-right:150px;
 font-weight:bold;
 font-size:1.2em;
-
 `;
-
 
 export const Headerlogo=styled.div`
-margin-top:15px;
-margin-left:80px;
+padding-top: 15px;
+display:flex;
+flex-direction:row;
 `;
+
 export const LImage=styled.img`
 width:50px;
 height:50px;

54405e874fa8e3d9a638f694961a609ef5e75170
Alwin John <47777599+Dexters-Hub@users.noreply.github.com>
2022-10-04 22:49:05 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Hero } from '../components/1-Hero/Hero';
 
 import Header from '../components/0-Header/Header'
 import Footer from '../components/z-Footer/Footer';
-import Initiatives from '../components/9-Initiative/initiative';
+import Initiatives from '../components/9-Initiative/Initiative';
 
 import { Layout } from '../layout/Layout';
 import Welcome from '../components/0-Header/Header';

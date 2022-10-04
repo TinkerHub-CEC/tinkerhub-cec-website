23439a491f22bfb130f77c8f3ec7dfd38539ed63
Abin-T-Sunil <abintsunil@gmail.com>
2022-10-04 10:03:44 +0530

    Added ContactUs and ContactUs.js Files

---
+++ src/components/Contact/ContactUs.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {Container,Heading,Container1,Block1,Block2,Heading1,Container2,Label1,Input1,MessButton,Textarea1,Box,Box1,Box2,Box3,Box4} from './ContactUsStyles'
+
+export const  Contact = () => {
+    return(
+        <Container>
+            <Heading>Connect with Us </Heading>
+            <Container1>
+             <Block1 src="/images/contact-us.png " alt="Contact us image"></Block1>
+             <Block2>
+                <Heading1>We'd love to hear from you.</Heading1>
+                <Container2>
+                  <Box>
+                    <Box1>
+                       <Label1 htmlFor="name">Your Name (required)</Label1>
+                       <Input1 type="text" id="name" name="name" placeholder="Your name" required/>
+                    </Box1>
+
+                    <Box2>
+                       <Label1 htmlFor="email">Your email (required)</Label1>
+                       <Input1 type="email" id="email" name="email" placeholder="Your Email" required/>
+                    </Box2>
+                  </Box>  
+                    
+                    <Box3>
+                       <Label1 htmlFor="category">Category (optional)</Label1>
+                       <Input1 type="text" id="category" name="category" placeholder="Category"/>
+                    </Box3>
+
+                    <Box4>
+                       <Label1 htmlFor="subject">Your Message</Label1>
+                       <Textarea1 id="subject" name="subject" placeholder="Message" ></Textarea1>
+                    </Box4>
+
+                    <MessButton type="submit" value="Send Message" >Send message</MessButton>
+                </Container2>
+             </Block2>
+            </Container1> 
+        </Container>
+    )
+}
+
+

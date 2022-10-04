dba5f32d0b295e438841e772f29d3bfc687370df
Alwin John <47777599+Dexters-Hub@users.noreply.github.com>
2022-10-04 22:42:33 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,30 +3,22 @@ import { Hero } from '../components/1-Hero/Hero';
 
 import Header from '../components/0-Header/Header'
 import Footer from '../components/z-Footer/Footer';
-<<<<<<< HEAD
 import Initiatives from '../components/9-Initiative/initiative';
-=======
->>>>>>> 4876dbe52029472df906fa4893fc0b3a6bd1c32e
 
 import { Layout } from '../layout/Layout';
 import Welcome from '../components/0-Header/Header';
 import About from './about'
+import { Contact } from '../components/Contact/ContactUs';
 
 const Home = () => {
   return (
     <Layout>
       <Welcome/>     
       <Hero/>
-<<<<<<< HEAD
       <Initiatives/>
+      <Contact/>
       <Footer />
       </Layout>
-      
-=======
-      <Footer />
-      </Layout>
-
->>>>>>> 4876dbe52029472df906fa4893fc0b3a6bd1c32e
     
   );
 };

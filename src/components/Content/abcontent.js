5e448f091fac2a848d3055a7e7708d061dc6ec92
l0gicerr0r <alfithomas2002@gmail.com>
2022-10-04 14:27:46 +0530

    initial commit

--- src/components/Content/abcontent.js
+++ src/components/Content/abcontent.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Container, HeroContentAction, HeroGrid, HeroImage, HeroParagraph, LargeHeading } from '../1-Hero/HeroStyles'
+import { Container, HeroContentAction, HeroGrid, HeroImage, HeroParagraph, LargeHeading } from './abcontentStyles'
 
-export const abcontent = () => {
+export const Abcontent = () => {
     return(
         <Container>
             <HeroGrid>
                 <HeroContentAction>
                     <LargeHeading>About Us</LargeHeading>
-                    <HeroParagraph>TinkerHub CEC is a Campus Community initiative of <a href ="https:// tinkethub.org/">https:// tinkethub.org/</a>at College Of Engineering Chengannur
+                    <HeroParagraph>TinkerHub CEC is a Campus Community initiative of <a href ="https:// tinkethub.org/">https:// tinkethub.org/</a> at <b>College Of Engineering Chengannur.</b></HeroParagraph>
 
-We at TinkerHub CE Chengannur aim to fulfill the mission and vision of Tinkerhub Foundation at our campus by empowering students with access to the knowledge they need to thrive in the new world
+<HeroParagraph>We at TinkerHub CE Chengannur aim to fulfill the mission and vision of Tinkerhub Foundation at our campus by empowering students with access to the knowledge they need to thrive in the new world.</HeroParagraph>
 
-TinkerHub Campus Community is for students who want to lear and teach technology We want our community members to understand and embrace the power of ortical thinking problem solving innovation, and entrepreneurship</HeroParagraph>
+<HeroParagraph>TinkerHub Campus Community is for students who want to lear and teach technology We want our community members to understand and embrace the power of ortical thinking problem solving innovation, and entrepreneurship.</HeroParagraph>
                 </HeroContentAction>
                 <HeroImage src="/images/hifi.png"/>
             </HeroGrid>
--- src/components/Content/abcontentStyles.js
+++ src/components/Content/abcontentStyles.js
@@ -1,19 +1,46 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
-export const LargeHeading = styled.div``;
+export const Container = styled.div`
+display: flex;
+
+flex-direction: column;
+margin-top: 4em;
+`;
+export const LargeHeading = styled.div`
+font-size: 4.2rem;
+font-weight: 700;
+width: 100%;
+`;
 export const HeroContentAction = styled.div`
-height
-width
+display: flex;
+width: auto;
+padding: 50px 0 0 50px;
+flex-direction: column;
 
 `;
 export const HeroImage = styled.div`
-position:relative;
-height
-width
+width: 500px;
+height: 500px;
+left-margin:1187px;
+right-margin:51px;
+top-margin:378;
+bottom-margin:3920;
 `;
-export const HeroGrid = styled.div``;
-export const HeroParagraph = styled.div``;
+
+export const HeroGrid = styled.div`
+
+display: flex;
+justify-content: space-around;
+margin: 0  0 3rem;
+`;
+export const HeroParagraph = styled.div`
+font-size: 1.6rem;
+
+width:60%;
+font-weight: 450;
+line-height: 1.5;
+margin: 1.5em 0;
+`;
 
 
 

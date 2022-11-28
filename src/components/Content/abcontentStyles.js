2bacce5b71c26174ab566902cc5737484a68e3e3
l0gicerr0r <alfithomas2002@gmail.com>
2022-11-28 19:49:13 +0530

    commit

--- src/components/Content/abcontentStyles.js
+++ src/components/Content/abcontentStyles.js
@@ -5,11 +5,24 @@ display: flex;
 
 flex-direction: column;
 margin-top: 4em;
+@media screen and (max-width:768px){
+    width:90em;
+
+}
 `;
+
+
 export const LargeHeading = styled.div`
 font-size: 4.2rem;
 font-weight: 700;
 width: 100%;
+@media screen and (max-width:768px){
+   
+    padding-left:0em;
+    font-size:70px;
+    word-spacing:0.3em;
+}
+
 `;
 export const HeroContentAction = styled.div`
 display: flex;
@@ -17,6 +30,12 @@ width: auto;
 padding: 50px 0 0 50px;
 margin-left:90px;
 flex-direction: column;
+@media screen and (max-width:768px){
+    font-size:4.2rem;
+
+    
+    
+}
 
 `;
 export const HeroImage = styled.img`
@@ -24,6 +43,15 @@ width: 681px;
 height: 651px
 margin-right:51px;
 padding-right:100px;
+@media screen and (max-width:768px){
+    width:50em;
+    height:50em;
+    
+    margin-left:350px;
+    
+    
+    
+}
 
 `;
 
@@ -32,7 +60,11 @@ export const HeroGrid = styled.div`
 display: flex;
 justify-content: space-around;
 margin: 0  0 3rem;
+@media screen and (max-width:768px){
+display:flex;
+flex-direction:column
 `;
+
 export const HeroParagraph = styled.div`
 font-size: 1.6rem;
 
@@ -40,6 +72,13 @@ width:70%;
 font-weight: 450;
 line-height: 1.5;
 margin: 1.5em 0;
+@media screen and (max-width:768px){
+    font-size: 6rem;
+    margin-left: 1px;
+    width: 90%;
+    
+
+
 `;
 
 

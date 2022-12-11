648d562db91b7107c6126d010b681f0cbad38905
Sreyas62 <sreyasmpillai2049@gmail.com>
2022-12-11 15:43:02 +0530

    newsletter-responsive

--- src/components/2-Newsletter/Newsletter.js
+++ src/components/2-Newsletter/Newsletter.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useState } from 'react';
 import {Box,Content,Heading,Form,Column,BlueButton} from './NewsletterStyles'
 
 export const Letter = () => {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const subscribeMe = async (event) => {
+        event.preventDefault();
+        console.log(email);
+        const res = await fetch("/api/subscribe", {
+          body: JSON.stringify({ email: email }),
+          headers: { 'Content-Type': 'application/json' },
+          method: "POST",
+        });
+
+        const { error, message } = await res.json();
+        setError(error);
+        setSuccess(message);
+      };
+
+    const changeEmail = (event) => {
+        const email = event.target.value;
+        setEmail(email);
+      }
+
     return (
         <Box>
             <Heading>
@@ -10,13 +34,22 @@ export const Letter = () => {
             <Content>
             Sign up to get emails about new posts and other info.<br></br>Unsubscribe anytime.
             </Content>
-            <Form>
+            <Form onSubmit={subscribeMe}>
                 
                 <br/><label htmlFor="email"></label>
-                <Column type="text" id="email" name="email" placeholder="yourname@gmail.com" /><br/><br/>
+                <Column 
+                onChange={changeEmail}
+                type="email" 
+                id="email" name="email"
+                placeholder="yourname@gmail.com" 
+                />
+                <br/><br/>
                 <BlueButton type="submit" value="GET UPDATES"/>
 	    
             </Form>
+            {success 
+          ? <span className="flex items-center text-sm font-bold text-green-700">{success}</span> 
+          : <span className="flex items-center text-sm font-bold text-red-800">{error}</span>}
         </Box>
     )
 }

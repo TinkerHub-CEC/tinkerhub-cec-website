f83cc12cfcc3be99c6b31ebba8da4c722cb787e8
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2023-02-08 16:13:53 +0530

    update removed

--- src/components/2-Newsletter/Newsletter.js
+++ src/components/2-Newsletter/Newsletter.js
@@ -1,31 +1,9 @@
 import React from "react";
-import { useState } from 'react';
+
 import {Box,Content,Heading,Form,Column,BlueButton} from './NewsletterStyles'
 
 export const Letter = () => {
-    const [email, setEmail] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
-
-    const subscribeMe = async (event) => {
-        event.preventDefault();
-        console.log(email);
-        const res = await fetch("/api/subscribe", {
-          body: JSON.stringify({ email: email }),
-          headers: { 'Content-Type': 'application/json' },
-          method: "POST",
-        });
-
-        const { error, message } = await res.json();
-        setError(error);
-        setSuccess(message);
-      };
-
-    const changeEmail = (event) => {
-        const email = event.target.value;
-        setEmail(email);
-      }
-
+    
     return (
         <Box>
             <Heading>
@@ -34,11 +12,11 @@ export const Letter = () => {
             <Content>
             Sign up to get emails about new posts and other info.<br></br>Unsubscribe anytime.
             </Content>
-            <Form onSubmit={subscribeMe}>
+            <Form>
                 
                 <br/><label htmlFor="email"></label>
                 <Column 
-                onChange={changeEmail}
+               
                 type="email" 
                 id="email" name="email"
                 placeholder="yourname@gmail.com" 
@@ -47,9 +25,7 @@ export const Letter = () => {
                 <BlueButton type="submit" value="GET UPDATES"/>
 	    
             </Form>
-            {success 
-          ? <span className="flex items-center text-sm font-bold text-green-700">{success}</span> 
-          : <span className="flex items-center text-sm font-bold text-red-800">{error}</span>}
+        
         </Box>
     )
 }

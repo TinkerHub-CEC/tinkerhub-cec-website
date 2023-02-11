1780e25a26f2f4b4c99ee0383fa53cd9bb788cf0
Rithin Samuel <rithinsamuel12@gmail.com>
2023-02-11 12:07:26 +0530

    Events added

--- src/components/4-Eventpage/EventStyles.js
+++ src/components/4-Eventpage/EventStyles.js
@@ -21,7 +21,7 @@ export const Button = styled.button`
   padding-right:4px;
   padding-top:2px;
   padding-bottom:2px;
-  @media screen and (max-width:768px){
+  @media screen and (max-width:720px){
     height:63px;
      `;
   
@@ -37,22 +37,24 @@ export const Button = styled.button`
 
 export const Card= styled.div`
     align-items: center;
-    border: 0.5px solid grey;
+    border: 0.5px solid lightgrey;
     max-height:1000px;
     width: 300px;
     margin-right:10px;
     margin-left: 10px;
  border-radius:10px;
  cursor: pointer;
- @media screen and (max-width:768px){
-  width:105rem;
+ position:relative;
+ box-shadow: 2px 2px 4px rgba(0.15,0.15,0.15,0.15);
+ @media screen and (max-width:720px){
+  width:50rem;
   height:100%;
   margin:10px;
-  
- }
+   }
 `;
 
 export const Title= styled.h2`
+        color: #4b2f00;
         margin-top: 12px;
         text-align: center;
          margin-bottom: 5px;
@@ -66,9 +68,9 @@ color: gray;
 display:inline-block;
 padding-left:10px;
 padding-right:20px;
-position: relative;left: 2em;
+position: relative;
+left: 0em;
 margin-bottom:5px;
-
 `;
 
 export const Paragraph= styled.p`
@@ -83,8 +85,9 @@ export const Card_image = styled.img`
     display:block;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
-    @media screen and (max-width:768px){
-  width:105rem;
+    box-shadow: 2px 2px 4px rgba(0.15,0.15,0.15,0.15);
+    @media screen and (max-width:720px){
+  width:50rem;
   height:40rem;
   
  }
@@ -92,4 +95,18 @@ export const Card_image = styled.img`
 `;
 export const All_buttons= styled.div`
 display:flex; 
+`;
+export const SpanElements=styled.div`
+max-width: 100%;
+width: 100%;
+display:block;
+float:left;
+text-align:center;
+align-items:start;
+justify-content: space-between;
+position: absolute;
+left:0;
+bottom:0;
+right:0;
+margin:auto;
 `;
